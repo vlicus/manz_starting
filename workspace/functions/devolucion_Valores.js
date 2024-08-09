b9be207f0df83a84a21818f6b8be28dcb884a857
Samuel Cobas <samu.cobas.ort@gmail.com>
2024-08-09 19:47:30 +0200

    finish devolucion_Valores.js

---
+++ workspace/functions/devolucion_Valores.js
@@ -0,0 +1,11 @@
+// Saving results in external variables (a + b into result)
+
+function addition(a, b){
+  return a + b;
+  //console.log("This will not be printed");
+}
+
+//Ejecution of the function
+const result = addition(2,3); // Storing value 5 in variable result.
+console.log("Resultado = ", result);
+

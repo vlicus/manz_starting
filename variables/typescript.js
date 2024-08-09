0986c85baa113d1041e2f8353898b606f4bb36bb
Samuel Cobas <samu.cobas.ort@gmail.com>
2024-08-09 17:17:58 +0200

    comment typescript.js: show error bc of typescript not set up

--- variables/typescript.js
+++ variables/typescript.js
@@ -1,7 +1,11 @@
 // Typescript will help to identify the type of the variable:
 
+/*
+
 const valueAsText : string = "50";
 const valueAsNumber : number = 50;
 
 console.log(valueAsText);
 console.log(valueAsNumber);
+
+*/

fe4f49e12ad4d0144e18a30d8c895f8986318fac
Samuel Cobas <samu.cobas.ort@gmail.com>
2024-08-09 17:01:55 +0200

    finish data_Type

---
+++ variables/data_Type.js
@@ -0,0 +1,19 @@
+// Data type
+
+// Static programming language vs Dinamic programming language:
+
+// Static programming language data type (character, number, etc) never changes after declaration.
+
+// Dinamic programming language data type can change during the development of the code.
+
+// JavaScript is a dinamic programming language.
+
+// Declaring the same value as different data types: (text, number and converting from text to number):
+
+const valueAsText = "50";
+const valueAsNumber = 50;
+const convertedValue = Number(valueAsText); // We can pass valueAsText or the value directly "50".
+
+console.log(valueAsText);
+console.log(valueAsNumber);
+console.log(convertedValue); // The value is converted to number from text.

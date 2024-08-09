29c33f4324e34360f11c316ee9263d0b6337e829
Samuel Cobas <samu.cobas.ort@gmail.com>
2024-08-09 17:06:08 +0200

    finish typescript.js

---
+++ variables/typescript.js
@@ -0,0 +1,7 @@
+// Typescript will help to identify the type of the variable:
+
+const valueAsText : string = "50";
+const valueAsNumber : number = 50;
+
+console.log(valueAsText);
+console.log(valueAsNumber);

e336e9aa71bcf9ac57d9d32d94f4f019a909d254
Samuel Cobas <samu.cobas.ort@gmail.com>
2024-08-13 21:13:54 +0200

    math_Objects edited

--- workspace/numbers/math_Object.js
+++ workspace/numbers/math_Object.js
@@ -20,28 +20,29 @@ Math.SQRT2    --> Equivalente a Math.sqrt(2)        -->	1.4142135623730951
 
 // Math methods.
 
-Math.abs(-5); // Absolute of -5 = 5
+Math.abs(-5);                         // Absolute of -5 = 5
 console.log(Math.abs(-5));
-Math.sign(-5);  // Sign of -5 (-1 negative, 1 positive) = -1
+Math.sign(-5);                        // Sign of -5 (-1 negative, 1 positive) = -1
 console.log(Math.sign(-5));
-Math.exp(1); // e^x --> e^1 (in this case) = 2.718281828459045
+Math.exp(1);                          // e^x --> e^1 (in this case) = 2.718281828459045
 console.log(Math.exp(1));
-Math.expm1(1); // e^x-1 --> e^1-1(in this case) = 1.718281828459045
+Math.expm1(1);                        // e^x-1 --> e^1-1(in this case) = 1.718281828459045
 console.log(Math.expm1(1));
-Math.max(1, 50, 30, 30.1, 50.1, 4); // choose the largest number (50.1)
+Math.max(1, 50, 30, 30.1, 50.1, 4);   // choose the largest number (50.1)
 console.log(Math.max(1, 50, 30, 30.1, 50.1, 4));
-Math.min(1, 50, 30, 30.1, 50,1, 4); // choose the shortest number (1)
+Math.min(1, 50, 30, 30.1, 50,1, 4);   // choose the shortest number (1)
 console.log(Math.min(1, 50, 30, 30.1, 50,1, 4));
-Math.pow(2, 10); // base^exp = 2^10 = 1024
+Math.pow(2, 10);                      // base^exp = 2^10 = 1024
 console.log(Math.pow(2, 10));
-Math.sqrt(2); // square root of x (2 in this case) = √x
+Math.sqrt(2);                         // square root of x (2 in this case) = √x
 console.log(Math.sqrt(2));
-Math.cbrt(2); // cube root of x (2 in this case) = √3x
+Math.cbrt(2);                         // cube root of x (2 in this case) = √3x
 console.log(Math.cbrt(2));
-Math.imul(0xffffffff, 7); // -7
+Math.imul(0xffffffff, 7);             // -7
 console.log(Math.imul(0xffffffff, 7));
 
 // Ejemplo de clz32 (count leading zeros)
+
 const x = 1;
 "0".repeat(Math.clz32(x)) + x.toString(2);
 console.log("0".repeat(Math.clz32(x)) + x.toString(2));
